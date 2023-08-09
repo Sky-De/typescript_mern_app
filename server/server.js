f8270d9ad2fa002fde58182a9cc6af733c6c9e25
SkyDe <sky_de1991@outlook.com>
2023-08-09 19:23:29 +0330

    production debugging

--- server/server.js
+++ server/server.js
@@ -13,7 +13,9 @@ env.config();
 const app = express();
 app.use(
   cors({
-    origin: true,
+    // origin: true,
+    origin:
+      "https://app.netlify.com/sites/sunny-cheesecake-4b7d8f/deploys/64d3b41d8e041c7d924a1a91",
     credentials: true,
     sameSite: "none",
     secure: true,
