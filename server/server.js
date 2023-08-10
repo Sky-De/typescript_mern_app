f48cffb69b15444894602f7eb27e7f6fd0d5d1cc
SkyDe <sky_de1991@outlook.com>
2023-08-10 19:33:29 +0330

    Access-Control-Allow-Origin added to server

--- server/server.js
+++ server/server.js
@@ -11,14 +11,6 @@ import errorHandler from "./middleware/errorHandler.js";
 
 env.config();
 const app = express();
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://sprightly-baklava-7dd4f2.netlify.app"
-  );
-  // You can also set other CORS headers here, if needed.
-  next();
-});
 app.use(
   cors({
     origin: true,
@@ -27,6 +19,31 @@ app.use(
     secure: true,
   })
 );
+
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
